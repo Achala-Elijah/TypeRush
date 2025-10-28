272587f7c2540a63031064a1050fdacd7dc73bbc
Elijah <eachala5@gmail.com>
2025-10-28 01:51:23 +0000

    added login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import Auth from './pages/Auth'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="text-red-500">
-        Hello
-        <Button variant="outline">Button</Button>
+      <div className="">
+       <Auth />
       </div>
     </>
   )
---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+
+
+function Auth(){
+    const [username, setUsername] = useState("")
+
+    const handleEnter = async (e) => {
+        e.preventDefault()
+        //Send username
+        //route to the typing page
+        console.log(username)
+    }
+
+
+    return(
+        <div className="max-w-[640px] flex flex-col items-center m-auto p-[40px] h-[600px]">
+            <form 
+            onSubmit={handleEnter}
+            className="flex flex-col items-center w-[100%] h-[100%]">
+                <label htmlFor="username" className="mb-[15px] text-[18px]">Enter your username</label>
+                <input 
+                id="username"
+                name="username"
+                type="text"
+                placeholder="Username"
+                autoComplete="off"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="text-center text-[#333] py-[6px] px-[12px] text-[2em] leading-[1.33] outline-none border border-black w-[calc(100%-26px)] h-[50px] mb-[18px]"
+                />
+                <button 
+                type="submit"
+                className="bg-[#3276b1] border-[#285e8e] text-white py-[10px] px-[16px] text-[18px] font-semibold rounded-[6px] font-sans">Enter</button>
+            </form>
+        </div>
+    )
+}
+
+
+export default Auth
